ae8fd1d56afe8c36c11bc22951d2ba55ff7292d5
madhan-innopay <madhan.kumar@innopay.in>
2024-08-29 17:13:26 +0530

    begin:initial commit

---
+++ innopay_dynamic_ui/lib/innopay_dynamic_ui.dart
@@ -0,0 +1,19 @@
+library innopay_dynamic_ui;
+
+import 'package:flutter/widgets.dart';
+
+/// A Calculator.
+class InnopayDynamicHomeScreen extends StatefulWidget {
+  const InnopayDynamicHomeScreen({super.key});
+
+  @override
+  State<InnopayDynamicHomeScreen> createState() =>
+      _InnopayDynamicHomeScreenState();
+}
+
+class _InnopayDynamicHomeScreenState extends State<InnopayDynamicHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("DYNAMIC INNOPAY UI");
+  }
+}
